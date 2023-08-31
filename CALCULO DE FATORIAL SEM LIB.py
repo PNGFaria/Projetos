8dd00fc87c1d212133c3995b767764cffcabe69c
Pedro Godoy <138706963+PNGFaria@users.noreply.github.com>
2023-08-31 00:57:40 -0300

    Programa de cálculo de fatorial sem biblioteca, utilizando função

---
+++ CALCULO DE FATORIAL SEM LIB.py
@@ -0,0 +1,16 @@
+#CONTADOR FATORIAL
+def fatorial(n, show=False):
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print('x', end='')
+            else:
+                print('=', end='')
+        f = f * c
+    return f
+
+
+num = int(input('Digite um número para mostrar o fatorial: '))
+print(fatorial(num, show=True))
