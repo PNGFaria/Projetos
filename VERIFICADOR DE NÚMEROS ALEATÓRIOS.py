fff1a2f1fb8283cdff798a53f9574745c981b1b4
Pedro Godoy <138706963+PNGFaria@users.noreply.github.com>
2023-08-31 00:57:57 -0300

    Programa de cálculo de fatorial sem biblioteca, utilizando função

---
+++ VERIFICADOR DE NÚMEROS ALEATÓRIOS.py
@@ -0,0 +1,21 @@
+#VERIFICADOR DE NÚMEROS ALEATÓRIOS
+from random import randint
+
+
+def sorteia(lista):
+    for c in range(0, 5):
+        lista.append(randint(1, 50))
+
+
+def somapar(lis):
+    soma = 0
+    for c in lis:
+        if c % 2 == 0:
+            soma = soma + c
+    print(soma)
+
+
+nums = list()
+sorteia(nums)
+print(f'Dentre os números {nums}, a soma entre os pares é: ')
+somapar(nums)
